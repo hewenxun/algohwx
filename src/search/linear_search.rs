ff0ccc81092172b1bb93ab8e169a4eb475a90c69
hewenxun <hewenxun@yeah.net>
2024-10-24 17:43:23 +0800

    linear search

---
+++ src/search/linear_search.rs
@@ -0,0 +1,9 @@
+pub fn linear_search<T: Eq>(items: &[T], target: &T) -> Option<usize>
+{
+    for (index, item) in items.iter().enumerate() {
+        if item.eq(target) {
+            return Some(index)
+        }
+    }
+    None
+}
